fa1579c26b23f211dfe019084749ee9c386082cd
Adam <55899222+UniverseCraft@users.noreply.github.com>
2023-04-12 16:57:00 -0500

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,4 +3,4 @@
 curl -o PowerWash.ps1 https://raw.githubusercontent.com/UniverseCraft/PowerWashPersonal/main/PowerWash.ps1
 curl -o PowerWashSettings.json https://raw.githubusercontent.com/UniverseCraft/PowerWashPersonal/main/PowerWashSettings.json
 Set-ExecutionPolicy Unrestricted -Force -Confirm:$false
-.\PowerWash.ps1 /config /noscans -Confirm:$false
+.\PowerWash.ps1 /config /noscans /is-unattend -Confirm:$false
